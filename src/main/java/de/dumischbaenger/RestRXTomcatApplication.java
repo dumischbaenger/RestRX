e9b40a0ec91023aee6ebe8f57df3fc9032ee6432
dumischbaenger <bdonauba@gmail.com>
2024-04-01 14:13:34 +0200

    Delete src/main directory

--- src/main/java/de/dumischbaenger/RestRXTomcatApplication.java
+++
@@ -1,9 +0,0 @@
-package de.dumischbaenger;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("apppath")
-public class RestRXTomcatApplication extends Application {
-
-}
